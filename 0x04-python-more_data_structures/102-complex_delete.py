3ba9551cc205ebde584490c517d9da7531d7a6b1
Samfield Hawb Bassey <samfield4sure@gmail.com>
2023-12-06 16:58:57 +0100

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new_dict = dict()
+    for key, v in a_dictionary.items():
+        print(v)
+        if v != value:
+            new_dict.setdefault(key, v)
+    return new_dict
