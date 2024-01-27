56889562ab50e553688f49a07ecf52fec5c3dfca
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-01-27 20:27:31 +0100

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for arr in matrix:
-        for idx,i in enumerate(arr):
-            print("{:d}".format(i), end="") if idx == len(arr) - 1 else print("{:d}".format(i), end=" ")
+        for idx, i in enumerate(arr):
+            print("{:d}".format(i), end="") if idx == len(arr) - 1 else print(
+                "{:d}".format(i), end=" "
+            )
         print()
