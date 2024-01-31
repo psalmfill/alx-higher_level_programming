b8fc4eed1d68ca3ef1529a9e609d24503bd64673
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-01-31 17:30:23 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # prepare the tuple
-    new_tuple_a = (tuple_a[i] if i < len(tuple_a) else 0 for i in range(0,2) ) 
-    new_tuple_b = (tuple_b[i] if i < len(tuple_b) else 0 for i in range(0,2) ) 
-    return tuple(map(sum,zip(new_tuple_a, new_tuple_b)))
+    new_tuple_a = (tuple_a[i] if i < len(tuple_a) else 0 for i in range(0, 2))
+    new_tuple_b = (tuple_b[i] if i < len(tuple_b) else 0 for i in range(0, 2))
+    return tuple(map(sum, zip(new_tuple_a, new_tuple_b)))
