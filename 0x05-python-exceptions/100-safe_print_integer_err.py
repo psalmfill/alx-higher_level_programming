853352014087558a66c40d2ba1e35e7947894843
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-02-01 10:38:17 +0100

    fix stderr print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception:", e)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
