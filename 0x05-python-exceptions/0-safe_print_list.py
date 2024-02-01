57930c22d93891c14089ecc31e8787fb64f83442
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-02-01 07:46:17 +0100

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    total_printed = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            total_printed += 1
+        print()
+        return total_printed
+    except:
+        print()
+        return total_printed
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+0-safe_print_list.py - a function that prints x elements of a list.
