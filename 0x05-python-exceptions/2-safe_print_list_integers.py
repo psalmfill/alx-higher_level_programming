d7d3cb9943f5e8401033cc6db0b4391784da095a
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-02-01 08:32:32 +0100

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     total_printed = 0
     try:
-        for i in range(0,x):
+        for i in range(0, x):
             if type(my_list[i]) is not int:
                 continue
             print("{:d}".format(my_list[i]), end="")
-            total_printed +=1
+            total_printed += 1
         print()
         return total_printed
     except Exception as e:
-       raise e
+        raise e
